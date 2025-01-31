1302a7c6f6db581083af4b78672e853430698521
trop3n <jasondkimm@proton.me>
2025-01-30 21:53:23 -0600

    helix lsp update

--- home-manager/modules/helix.nix
+++ home-manager/modules/helix.nix
@@ -9,16 +9,75 @@
         select = "underline";
       };
     };
-    languages.language = [{
-      name = "nix";
-      auto-format = true;
-      formatter.command = lib.getExe pkgs.nixfmt-rfc-style;
-    }];
-    # themes = {
-    #   monokai_pro_spectrum = {
-    #     "inherits" = "monokai_pro";
-    #     "ui.background" = { };
-    #   };
-    # };
+    languages.language = [
+      {
+        name = "nix";
+        auto-format = true;
+        formatter.command = lib.getExe pkgs.nixfmt-rfc-style;
+      }
+        # Python
+      {
+        name = "python";
+        auto-format = true;
+        formatter.command = lib.getExe pkgs.black;
+        language-servers = [ { command = lib.getExe pkgs.python3Packages.python-lsp-server; } ];
+      }
+      # Rust
+      {
+        name = "rust";
+        auto-format = true;
+        formatter.command = lib.getExe pkgs.rustfmt;
+        language-servers = [ { command = lib.getExe pkgs.rust-analyzer; } ];
+      }
+      # JavaScript/TypeScript
+      {
+        name = "javascript";
+        auto-format = true;
+        formatter.command = lib.getExe pkgs.nodePackages.prettier;
+        language-servers = [ { command = lib.getExe pkgs.nodePackages.typescript-language-server; } ];
+      }
+      {
+        name = "typescript";
+        auto-format = true;
+        formatter.command = lib.getExe pkgs.nodePackages.prettier;
+        language-servers = [ { command = lib.getExe pkgs.nodePackages.typescript-language-server; } ];
+      }
+      # C
+      {
+        name = "c";
+        auto-format = true;
+        formatter.command = "${pkgs.clang-tools}/bin/clang-format";
+        language-servers = [ { command = "${pkgs.clang-tools}/bin/clangd"; } ];
+      }
+      # C++
+      {
+        name = "cpp";
+        auto-format = true;
+        formatter.command = "${pkgs.clang-tools}/bin/clang-format";
+        language-servers = [ { command = "${pkgs.clang-tools}/bin/clangd"; } ];
+      }
+      # Ruby
+      {
+        name = "ruby";
+        auto-format = true;
+        formatter.command = lib.getExe pkgs.rubocop;
+        language-servers = [ { command = lib.getExe pkgs.solargraph; } ];
+      }
+      # Gleam
+      {
+        name = "gleam";
+        auto-format = true;
+        formatter.command = lib.getExe pkgs.gleam;
+        language-servers = [ { command = lib.getExe pkgs.gleam; args = [ "lsp" ]; } ];
+      }
+      # Elixir
+      {
+        name = "elixir";
+        auto-format = true;
+        formatter.command = "mix";
+        formatter.args = [ "format" ];
+        language-servers = [ { command = lib.getExe pkgs.elixir_ls; } ];
+      }   
+    ];
   };
 }
